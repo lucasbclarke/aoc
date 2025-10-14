12d1ae3cbc6b248c896029fa839354f34f06a2d6
lucasbclarke <lucasbaileyclarke@outlook.com>
2025-10-14 19:05:41 +1100

    added a print message and removed zig-out

--- 2024/Day-1/Part-1/src/main.zig
+++ 2024/Day-1/Part-1/src/main.zig
@@ -275,6 +275,8 @@ pub fn main() !void {
                         //add the number to the index notation
                     }
                 }
+            } else {
+                 print("could not find key, seq1 = {d}", .{seq1});
             }
         }
         print("newline\n", .{});
